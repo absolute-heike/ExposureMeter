1740fe3eff5e34648305195005f477fdbfd8216a
Michael Berg <michael.berg@lovoo.com>
2017-12-24 14:53:04 +0100

    Initial Commit

---
+++ ExposureTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExposureTracker
+//
+//  Created by Michael Berg on 24.12.17.
+//  Copyright © 2017 Michael Berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
